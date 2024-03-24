24fae3c258dcbe49be3c5465399e86923b259cb0
yeshuoer <xlsapple@outlook.com>
2024-03-24 17:30:57 +0800

    lint error

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,6 +1,6 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
 
-export default function ({ }) {
+export default function Navigation() {
   return <div>
     <div className="flex items-center justify-between mx-40 pt-5">
       <h1 className="text-blue-600 text-3xl">AI NFT Generator</h1>
